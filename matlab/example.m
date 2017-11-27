521633c43193c1897399f289d539e0a35ab74153
yurun <leonbel@163.com>
2017-11-27 17:35:44 +0800

    Update example.m

--- matlab/example.m
+++ matlab/example.m
@@ -6,10 +6,10 @@ flagAug = 1;
 flagGPU = 1;
 batchSize = 1000;
 
-if flagCS   %if CS structure is used , testdata should be in size of 64*64*1*N 
-    testPatch = rand(64,64,1,10,'single');
+if flagCS   %if CS structure is used, testdata should be in size of 64*64*1*N 
+    testPatch = randi([0 255],64,64,1,10,'single');%the pixel value of the input patches shoud be in range of 0 tp 255
 else
-    testPatch = rand(32,32,1,10,'single');
+    testPatch = randi([0 255],32,32,1,10,'single');
 end
 
 desFloat = cal_L2Net_des(rootPath,trainSet,flagCS,flagAug,testPatch,batchSize,flagGPU);%output a 128(or 256)*N matrix, each colum is a descriptor
